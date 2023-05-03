a599a8187afc8931592e5545b44773f98fad8f85
Andriy-Kolokolov <koloandre22@gmail.com>
2023-05-03 12:05:01 +0200

    add img objects array

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,22 @@
-const arrImages = [
-    'assets/img/01.webp',
-    'assets/img/02.webp',
-    'assets/img/03.webp',
-    'assets/img/04.webp',
-    'assets/img/05.webp',
-    'https://www.freecodecamp.org/news/content/images/2022/08/pexels-antonio-batinic--4164418--1-.jpg'
-];
-
 const images = [
     {
-        image: 'img/01.webp',
+        image: 'assets/img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: 'assets/img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: 'assets/img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: 'assets/img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: 'assets/img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }, {
@@ -38,11 +29,10 @@ const images = [
 const containerHighlighted = document.querySelector('.highlighted');
 const containerThumbs = document.querySelector('.thumbs');
 
-for (let i = 0; i < arrImages.length; i++) {
-    containerHighlighted.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
-    containerThumbs.innerHTML += `<img src="${arrImages[i]}" alt="" class="${i == 0 ? 'active' : ''}">`;
-}
-
+images.forEach((image, index) => {
+    containerHighlighted.innerHTML += `<img src="${image.image}" alt="" class="${index == 0 ? 'active' : ''}">`;
+    containerThumbs.innerHTML += `<img src="${image.image}" alt="" class="${index == 0 ? 'active' : ''}">`;
+});
 
 // selezionimo le immagini nell'html
 const listHighlighted = document.querySelectorAll('.highlighted img');
